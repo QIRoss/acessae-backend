c5bfa21a2eb51676ec3bae39b6a2626368ad850b
qiross <lucasqreis@poli.ufrj.br>
2021-08-31 00:04:13 -0300

    :)

---
+++ lighthouseApp.js
@@ -0,0 +1,22 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+const getLighthouseAccessibilityScore = async (url) => {
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const options = {logLevel: 'info', output: 'html', onlyCategories: ['accessibility'], port: chrome.port};
+  const runnerResult = await lighthouse(url, options);
+  console.log(runnerResult.lhr.categories)
+  const finalUrl = runnerResult.lhr.finalUrl;
+  const accessibilityScore = runnerResult.lhr.categories.accessibility.score * 100;
+
+  console.log('Report is done for', finalUrl);
+  console.log('Accessibility score was', accessibilityScore);
+
+  await chrome.kill();
+  return {
+    finalUrl: finalUrl,
+    AccessibilityScore: accessibilityScore
+  }
+}
+
+module.exports = getLighthouseAccessibilityScore;
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const getLighthouseaccessibilityScore = require('./lighthouseApp')
+
+const app = express();
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const lighthouseaccessibilitySchema = new mongoose.Schema({
+    accessibility: Number,
+    searches: {
+            type: Number,
+            default: 1
+        },
+    usersVoted: {type: Number},
+    userScore: {type: Number},
+    comments: {
+        type: Array,
+        default: []
+    },
+    url: String
+});
+
+const Lighthouseaccessibility = mongoose.model('Lighthouseaccessibility', lighthouseaccessibilitySchema);
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.route('/api/accessibility')
+    .get((req, res) => {
+        Lighthouseaccessibility.find({}, (err, data) => {
+            res.json(data);
+        });
+    })
+    .post(async (req, res) => {
+        const url = req.body.url;
+
+        const lighthouseResults = await getLighthouseaccessibilityScore(url);
+
+        lighthouseaccessibilitySchema.findOneAndUpdate(
+            {url: lighthouseResults.finalUrl}, 
+            {accessibility: lighthouseResults.accessibilityScore, $inc: {searches: 1}}, 
+            {useFindAndModify: false, new: true}, 
+            (err, data) => {
+                if (!data){
+                    data = new Lighthouseaccessibilitys(lighthouseResults);
+                    console.log("New site accessibility created")
+                }
+                data.save((err) => {
+                    if(!error){
+                        console.log("Save success");
+                    }
+                });
+                res.json(data);
+            });
+        // res.json(lighthouseResults);
+});
+
+app.listen(3001, () => {
+    console.log("server is online");
+})
