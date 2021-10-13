ea84f4ac65f3364035b8c1d1ee1e52a24e04280e
Camilo <camilo.carneiro@estrategiaconcursos.com.br>
2021-10-13 17:13:36 -0300

    corrige erro em commits anteriores

--- src/entities.js
+++ src/entities.js
@@ -3,13 +3,16 @@ const Comment = {
   name: String,
   body: String,
   created_at: String,
-  score: Number,
+  score: String,
 };
 
 const Response = {
   id: String,
   a11y_score: String,
-  user_score: String,
+  user_score: {
+    type: String,
+    default: "S/N",
+  },
   hits: Number,
   url: String,
   comments: [Comment],
